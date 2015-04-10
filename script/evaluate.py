7563075fb9555c6406bd5508702a71b23df7acc5
micolin <micolin@e4-System-Product-Name.(none)>
2015-04-10 21:06:40 +0800

    Test

--- script/evaluate.py
+++ script/evaluate.py
@@ -1,21 +1,23 @@
 #coding = utf8
 import sys,os
 import json
+from databuilder import get_test_data
+from collections import *
 
 class Evaluator(object):
-	def __init__(self):
-		self.rec_result = {}
-		self.test_result = {}
+	def __init__(self,rec_result,test_result):
+		self.rec_result = rec_result
+		self.test_result = test_result
 	
 	def score(self):
 		hit = 0
 		predict_tot = 0
 		recall_tot = 0
-		for uid, predict_songs in self.rec_result.iteritems():
-			test_songs = self.test_set[uid]
-			hit += len(set(predict_songs) & set(test_songs))
-			predict_tot += len(predict_songs)
-			recall_tot += len(test_songs)
+		for uid, predict_items in self.rec_result.iteritems():
+			test_items = self.test_result[uid]
+			hit += len(set(predict_items) & set(test_items))
+			predict_tot += len(predict_items)
+			recall_tot += len(test_items)
 		
 		precision = float(hit)/predict_tot
 		recall = float(hit)/recall_tot
@@ -26,4 +28,20 @@ class Evaluator(object):
 
 		scores = {'precision':precision,'recall':recall,'f_score':fscore}
 		return scores
-		
+
+def evaluate(prefile,test_date):
+	rec_result = defaultdict(list)
+	with open(prefile,'rb') as fin:
+		for line in fin.readlines():
+			line = line.strip().split('\t')
+			uid = line[0]
+			pre_items = line[1].split(',')
+			rec_result[uid]=pre_items
+
+	test_data = get_test_data(test_date)
+	evaluator = Evaluator(rec_result,test_data)
+	print evaluator.score()
+
+if __name__=='__main__':
+	evaluate('../rec_result/tianchi_mobile_recommendation_predict','2014-12-18')
+			
--- script/popularity.py
+++ script/popularity.py
@@ -6,7 +6,8 @@ from collections import *
 from databuilder import *
 
 def popularity():
-	user_view,user_collection,user_basket,user_purchase=get_resent_actions('2014-12-18',10)
+	r_days = 10
+	user_view,user_collection,user_basket,user_purchase=get_resent_actions('2014-12-18',r_days)
 	user_itemRate = defaultdict(dict)
 	view_rate = 0.2
 	collection_rate = 0.5
@@ -18,10 +19,39 @@ def popularity():
 				user_itemRate[uid][item] += cnt*view_rate
 			except:
 				user_itemRate[uid][item] = cnt*view_rate
-		break
-	print user_itemRate
-	return user_itemRate
 
+	for uid,items in user_collection.items():
+		for item,cnt in items.items():
+			try:
+				user_itemRate[uid][item] += cnt*collection_rate
+			except:
+				user_itemRate[uid][item] = cnt*collection_rate
+	
+	for uid,items in user_basket.items():
+		for item,cnt in items.items():
+			try:
+				user_itemRate[uid][item] += cnt*basket_rate
+			except:
+				user_itemRate[uid][item] = cnt*basket_rate
+	
+	user_buy_cnt = defaultdict(int)
+	for uid,items in user_purchase.items():
+		user_buy_cnt[uid] += len(items.keys())
+		for item,cnt in items.items():
+			try:
+				user_itemRate[uid][item] += cnt*purchase_rate
+			except:
+				user_itemRate[uid][item] = cnt*purchase_rate
+
+	for uid,items in user_itemRate.items():
+		sort_items = sorted(items.items(),key=lambda x:x[1],reverse=True)
+		rec_num = user_buy_cnt[uid]/r_days
+		if rec_num == 0:
+			rec_num = 2
+
+		rec_items = [item[0] for item in sort_items[:rec_num]]
+		print "%s\t%s"%(uid,','.join(rec_items))
+	return user_itemRate
 
 if __name__=="__main__":
 	logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)s %(funcName)s %(lineno)d %(message)s')
